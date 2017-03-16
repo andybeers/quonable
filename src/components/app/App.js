448fc94aa0008c51e942713911974acaaf2de246
Andrew Beers <andybeers@gmail.com>
2017-03-16 13:43:10 -0700

    Adding basic component file structure

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Quote from './Quote';
 
 class App extends Component {
   render() {
     return (
       <div>
-        top level app component
+        <Quote />
       </div>
     );
   }
