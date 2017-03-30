cc2362c7769f94a8d84889726d2f05430d824d4f
Andrew Beers <andybeers@gmail.com>
2017-03-30 15:56:16 -0700

    Main content expanded so that footer only slightly appears at bottom. Styled footer.

--- src/components/AppFooter/AppFooter.js
+++ src/components/AppFooter/AppFooter.js
@@ -4,12 +4,14 @@ import './AppFooter.css';
 const AppFooter = () => {
   return (
     <footer>
-      <h3>quone</h3>
-      <h3>
-        verb <br/>
-        1. A neologism coined in "The Stakeout" episode of the television series "Seinfeld", "quone" was a word used by Mrs. Seinfeld in a Scrabble game. It was found by Jerry to not be a real word, but Kramer insists that it means "to sedate".
-      </h3>
-      <h3><i>"This dictionary's no good, we need a medical dictionary! When a patient is difficult, you quone him." - Kramer</i></h3> 
+      <div>
+        <h3>quone</h3>
+        <p>
+          verb <br/>
+          1. A neologism coined in "The Stakeout" episode of the television series "Seinfeld", "quone" was a word used by Mrs. Seinfeld in a Scrabble game. It was found by Jerry to not be a real word, but Kramer insists that it means "to sedate".
+        </p>
+        <p><i>"This dictionary's no good, we need a medical dictionary! When a patient is difficult, you quone him." - Kramer</i></p> 
+      </div>
     </footer>
   );
 };
