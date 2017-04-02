2188f98b2ce06845d8b91049da4b27275737ad25
Andrew Beers <andybeers@gmail.com>
2017-04-01 23:28:23 -0700

    Passing both serious and goofy data to quote component. Quote component holds view state.

--- src/components/quote/quote.js
+++ src/components/quote/quote.js
@@ -4,11 +4,13 @@ import './Quote.css';
 class Quote extends Component {
 
   render() {
+    let QUOTE_VIEW = this.props.showSeriousQuote ? 'quoteReal' : 'quoteGoofy';
+    let AUTH_VIEW = this.props.showSeriousQuote ? 'quoteGoofy' : 'quoteReal';
     return (
       <main>
         <div className='wrapper'>
-          <p>{this.props.quote}</p>
-          <p className='author'>- {this.props.author}</p>
+          <p>{this.props[QUOTE_VIEW].quote}</p>
+          <p className='author'>- {this.props[AUTH_VIEW].author}</p>
         </div>
         <div className='controls'>
           <button onClick={this.props.newQuote}>NEW QUOTE</button>
