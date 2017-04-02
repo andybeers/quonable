339f01120e6c7348dcb42ebdd75a788f4df16126
Andrew Beers <andybeers@gmail.com>
2017-04-02 12:51:54 -0700

    Quote already recieves matched quotes and authors

--- src/components/Quote/Quote.js
+++ src/components/Quote/Quote.js
@@ -5,12 +5,11 @@ class Quote extends Component {
 
   render() {
     let QUOTE_VIEW = this.props.showSeriousQuote ? 'quoteReal' : 'quoteGoofy';
-    let AUTH_VIEW = this.props.showSeriousQuote ? 'quoteGoofy' : 'quoteReal';
     return (
       <main>
         <div className='wrapper'>
           <p>{this.props[QUOTE_VIEW].quote}</p>
-          <p className='author'>- {this.props[AUTH_VIEW].author}</p>
+          <p className='author'>- {this.props[QUOTE_VIEW].author}</p>
         </div>
         <div className='controls'>
           <button onClick={this.props.newQuote}>NEW QUOTE</button>
