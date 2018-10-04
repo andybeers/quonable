1e105f7bbfd3df44fb031351b20dfeadb81059d0
Andrew Beers <andybeers@gmail.com>
2018-10-03 21:26:11 -0700

    Dont pass props when we can connect the component directly

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,42 +1,14 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
+import React from 'react'
 
 import Quote from '../Quote'
 import AppFooter from '../AppFooter'
-import { quoteActionCreators } from '../../dux'
 import './App.css'
 
-class App extends Component {
-  componentDidMount() {
-    this.props.generateQuote(this.props.goofyQuote)
-  }
-
-  render() {
-    const { text, author, toggleView, generateQuote } = this.props
-
-    return (
-      <div>
-        <Quote
-          text={text}
-          author={author}
-          toggleView={toggleView}
-          generateQuote={generateQuote}
-        />
-        <AppFooter />
-      </div>
-    )
-  }
+export default function App() {
+  return (
+    <div>
+      <Quote />
+      <AppFooter />
+    </div>
+  )
 }
-
-const mapStateToProps = store => ({
-  text: store.text,
-  author: store.author,
-  goofyQuote: store.goofyQuote
-})
-
-const mapDispatchToProps = {
-  generateQuote: quoteActionCreators.generateQuote,
-  toggleView: quoteActionCreators.toggleView
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(App)
--- src/components/Quote/index.jsx
+++ src/components/Quote/index.jsx
@@ -1,51 +1,67 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { string, func } from 'prop-types'
+import { bool, func, string } from 'prop-types'
 
 import AppHeader from '../AppHeader'
 import Button from '../Button'
 import './Quote.css'
+import { quoteActionCreators } from '../../dux'
 
-const propTypes = {
-  text: string.isRequired,
-  author: string.isRequired,
-  generateQuote: func.isRequired,
-  toggleView: func.isRequired,
+class Quote extends Component {
+  static propTypes = {
+    author: string.isRequired,
+    generateQuote: func.isRequired,
+    goofyQuote: bool.isRequired,
+    text: string.isRequired,
+    toggleView: func.isRequired,
+  }
+
+  componentDidMount() {
+    const { generateQuote, goofyQuote } = this.props
+
+    generateQuote(goofyQuote)
+  }
+
+  render() {
+    const { author, generateQuote, goofyQuote, text, toggleView } = this.props
+    return (
+      <main className={`quote-body-${goofyQuote ? 'a' : 'b'}`}>
+        <AppHeader />
+        <div className="w-80-ns mw-4-ns mv4 pa4 center">
+          <blockquote className="athelas ml0 mt0 pl4 bl bw2 b--light-blue">
+            <p className="f4 f3-m f2-l lh-copy mt0 white">{text}</p>
+            <cite className="f5 ttu tracked fs-normal authorName">- {author}</cite>
+          </blockquote>
+        </div>
+        <div className="pa4 mt2 sans-serif">
+          <Button onClick={() => generateQuote(goofyQuote)}>New Quote</Button>
+          <Button
+            className="ml3"
+            onClick={() => {
+              generateQuote(!goofyQuote)
+              toggleView()
+            }}
+          >
+            Invert Wisdom
+          </Button>
+        </div>
+      </main>
+    )
+  }
 }
 
-function Quote(props) {
-  return (
-    <main className={`quote-body-${props.goofyQuote ? 'a' : 'b'}`}>
-      <AppHeader />
-      <div className="w-80-ns mw-4-ns mv4 pa4 center">
-        <blockquote className="athelas ml0 mt0 pl4 bl bw2 b--light-blue">
-          <p className="f4 f3-m f2-l lh-copy mt0 white">{props.text}</p>
-          <cite className="f5 ttu tracked fs-normal authorName">
-            - {props.author}
-          </cite>
-        </blockquote>
-      </div>
-      <div className="pa4 mt2 sans-serif">
-        <Button onClick={() => props.generateQuote(props.goofyQuote)}>
-          New Quote
-        </Button>
-        <Button
-          className="ml3"
-          onClick={() => {
-            props.generateQuote(!props.goofyQuote)
-            props.toggleView()
-          }}
-        >
-          Invert Wisdom
-        </Button>
-      </div>
-    </main>
-  )
-}
-
-const mapStateToProps = state => ({
-  goofyQuote: state.goofyQuote,
+const mapStateToProps = store => ({
+  author: store.author,
+  text: store.text,
+  goofyQuote: store.goofyQuote,
 })
 
-Quote.propTypes = propTypes
-export default connect(mapStateToProps)(Quote)
+const mapDispatchToProps = {
+  generateQuote: quoteActionCreators.generateQuote,
+  toggleView: quoteActionCreators.toggleView,
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(Quote)
