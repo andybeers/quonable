d1befd2be09ac6b98a04b976543d78b35c472ab8
Andrew Beers <andybeers@gmail.com>
2018-10-06 14:52:04 -0700

    Make roll a recursive helper to prevent duplicate data

--- src/dux.js
+++ src/dux.js
@@ -1,77 +1,123 @@
 import { combineReducers } from 'redux'
+import { roll } from './utils/quote-helpers'
 
+// DATA FILES
+// ------------------------------------
 import authorData from './authors'
 import quoteData from './quotes'
 
+// ACTION CONSTANTS
+// ------------------------------------
 const TOGGLE_VIEW = 'TOGGLE_VIEW'
 const GENERATE_QUOTE = 'GENERATE_QUOTE'
 
-const text = (state = '', action) => {
-  switch (action.type) {
+// REDUCERS
+// ------------------------------------
+const text = (state = '', { text, type }) => {
+  switch (type) {
     case GENERATE_QUOTE:
-      return action.text
+      return text
     default:
       return state
   }
 }
-
-const author = (state = '', action) => {
-  switch (action.type) {
+const author = (state = '', { author, type }) => {
+  switch (type) {
     case GENERATE_QUOTE:
-      return action.author
+      return author
     default:
       return state
   }
 }
-
-const goofyQuote = (state = true, action) => {
-  switch (action.type) {
+const goofyQuote = (state = true, { type }) => {
+  switch (type) {
     case TOGGLE_VIEW:
       return !state
     default:
       return state
   }
 }
+const indexData = (
+  state = { authorIndex: 0, quoteIndex: 0 },
+  { authorIndex, quoteIndex, type },
+) => {
+  switch (type) {
+    case GENERATE_QUOTE:
+      return { authorIndex, quoteIndex }
+    default:
+      return state
+  }
+}
 
 const rootReducer = combineReducers({
-  text,
   author,
   goofyQuote,
+  indexData,
+  text,
 })
 
 export default rootReducer
 
-function roll(maxRange) {
-  return Math.floor(Math.random() * maxRange)
-}
-
-export const generateQuote = goofyQuote => {
-  let quoteIndex, authorIndex, text, author
-
-  if (goofyQuote) {
-    quoteIndex = roll(quoteData.goofy.length)
-    authorIndex = roll(authorData.real.length)
-    text = quoteData.goofy[quoteIndex].text
-    author = authorData.real[authorIndex].name
-  } else {
-    quoteIndex = roll(quoteData.real.length)
-    authorIndex = roll(authorData.goofy.length)
-    text = quoteData.real[quoteIndex].text
-    author = authorData.goofy[authorIndex].name
-  }
+// ACTION CREATORS
+// ------------------------------------
+export const getGoofyQuote = ({
+  authorIndex: prevAuthIndex,
+  quoteIndex: prevQuoteIndex,
+} = {}) => {
+  const quoteIndex = roll(quoteData.goofy.length, prevQuoteIndex)
+  console.log({ quoteIndex })
+  const authorIndex = roll(authorData.real.length, prevAuthIndex)
+  console.log({ authorIndex })
+  const text = quoteData.goofy[quoteIndex].text
+  const author = authorData.real[authorIndex].name
 
   return {
     type: GENERATE_QUOTE,
-    text: text,
-    author: author,
+    author,
+    authorIndex,
+    quoteIndex,
+    text,
   }
 }
 
-export const toggleView = () => {
-  return { type: TOGGLE_VIEW }
+export const getRealQuote = ({
+  authorIndex: prevAuthIndex,
+  quoteIndex: prevQuoteIndex,
+} = {}) => {
+  const quoteIndex = roll(quoteData.real.length, prevQuoteIndex)
+  const authorIndex = roll(authorData.goofy.length, prevAuthIndex)
+  const text = quoteData.real[quoteIndex].text
+  const author = authorData.goofy[authorIndex].name
+
+  return {
+    type: GENERATE_QUOTE,
+    author,
+    authorIndex,
+    quoteIndex,
+    text,
+  }
 }
 
-export const quoteActionCreators = {
-  generateQuote,
+export const toggleView = () => ({ type: TOGGLE_VIEW })
+
+export const actionCreators = {
+  getGoofyQuote,
+  getRealQuote,
   toggleView,
+}
+
+// SELECTORS
+// ------------------------------------
+const getAuthor = store => store.author
+const getAuthorIndex = store => store.indexData.authorIndex
+const getQuote = store => store.text
+const getQuoteIndex = store => store.indexData.quoteIndex
+const getQuoteState = store => store.goofyQuote
+
+export const selectors = {
+  getAuthor,
+  getAuthorIndex,
+  getQuoteIndex,
+  getQuoteState,
+  getQuote,
 }
