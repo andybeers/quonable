7078165ef22615e6dd29b7b18a8f6d81f25e87f6
Andrew Beers <andybeers@gmail.com>
2019-02-18 16:34:56 -0800

    Updated favicon, added useEffect hooks, upgraded react, mobile styles button padding change

--- src/components/AppFooter/index.jsx
+++ src/components/AppFooter/index.jsx
@@ -23,7 +23,7 @@ function AppFooter() {
         </p>
         <br />
         <p className="moon-gray copy">
-          ANDY BEERS &copy; 2017
+          ANDY BEERS
           <a href="https://github.com/andybeers/quonable" className="ml2 link dim">
             GITHUB
           </a>
--- src/components/Quote/index.jsx
+++ src/components/Quote/index.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useEffect } from 'react'
 import { connect } from 'react-redux'
 import { bool, func, number, string } from 'prop-types'
 
@@ -7,66 +7,66 @@ import Button from '../Button'
 import { actionCreators, selectors } from '../../dux/dux'
 import './Quote.css'
 
-class Quote extends Component {
-  static propTypes = {
-    author: string.isRequired,
-    authorIndex: number.isRequired,
-    getGoofyQuote: func.isRequired,
-    getRealQuote: func.isRequired,
-    goofyQuote: bool.isRequired,
-    quoteIndex: number.isRequired,
-    text: string.isRequired,
-    toggleView: func.isRequired,
-  }
-
-  componentDidMount() {
-    const { getGoofyQuote } = this.props
-
-    getGoofyQuote()
-  }
-
-  render() {
-    const {
-      author,
-      authorIndex,
-      getGoofyQuote,
-      getRealQuote,
-      goofyQuote,
-      quoteIndex,
-      text,
-      toggleView,
-    } = this.props
-
-    const fetchQuote = goofyQuote ? getGoofyQuote : getRealQuote
-    const fetchOpposite = goofyQuote ? getRealQuote : getGoofyQuote
-
-    return (
-      <main className={`quote-body-${goofyQuote ? 'a' : 'b'}`}>
-        <AppHeader />
-        <div className="w-80-ns mw-4-ns mv4 pa4 center">
-          <blockquote className="athelas ml0 mt0 pl4 bl bw2 b--light-blue">
-            <p className="f4 f3-m f2-l lh-copy mt0 white">{text}</p>
-            <cite className="f5 ttu tracked fs-normal authorName">- {author}</cite>
-          </blockquote>
-        </div>
-        <div className="pa4 mt2 mb2 sans-serif">
-          <Button onClick={() => fetchQuote({ authorIndex, quoteIndex })}>
-            New Quote
-          </Button>
-          <Button
-            className="ml3"
-            onClick={() => {
-              toggleView()
-              fetchOpposite()
-            }}
-          >
-            Invert Wisdom
-          </Button>
-        </div>
-      </main>
-    )
-  }
+const propTypes = {
+  author: string.isRequired,
+  authorIndex: number.isRequired,
+  getGoofyQuote: func.isRequired,
+  getRealQuote: func.isRequired,
+  goofyQuote: bool.isRequired,
+  quoteIndex: number.isRequired,
+  text: string.isRequired,
+  toggleView: func.isRequired,
 }
+
+const Quote = ({
+  author,
+  authorIndex,
+  getGoofyQuote,
+  getRealQuote,
+  goofyQuote,
+  quoteIndex,
+  text,
+  toggleView,
+}) => {
+  // Load quote on mount
+  // Empty array `useEffect` argument tells React to only call this once
+  useEffect(() => {
+    getGoofyQuote()
+  }, [])
+
+  const fetchQuote = goofyQuote ? getGoofyQuote : getRealQuote
+  const fetchOpposite = goofyQuote ? getRealQuote : getGoofyQuote
+
+  return (
+    <main className={`quote-body-${goofyQuote ? 'a' : 'b'}`}>
+      <AppHeader />
+      <div className="w-80-ns mw-4-ns mv4 pa4 center">
+        <blockquote className="athelas ml0 mt0 pl4 bl bw2 b--light-blue">
+          <p className="f4 f3-m f2-l lh-copy mt0 white">{text}</p>
+          <cite className="f5 ttu tracked fs-normal authorName">- {author}</cite>
+        </blockquote>
+      </div>
+      <div className="pa4 mt2 mb2 sans-serif">
+        <Button
+          className="mr3"
+          onClick={() => fetchQuote({ authorIndex, quoteIndex })}
+        >
+          New Quote
+        </Button>
+        <Button
+          onClick={() => {
+            toggleView()
+            fetchOpposite()
+          }}
+        >
+          Invert Wisdom
+        </Button>
+      </div>
+    </main>
+  )
+}
+
+Quote.propTypes = propTypes
 
 const mapStateToProps = store => ({
   author: selectors.getAuthor(store),
